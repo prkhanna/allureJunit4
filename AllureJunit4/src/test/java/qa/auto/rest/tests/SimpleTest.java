74f2d6395f1c3e83658202b8ada3abf0ea0798ef
prkhanna <prince.khanna1988@gmail.com>
2020-09-13 19:40:13 +0530

    first commit

---
+++ AllureJunit4/src/test/java/qa/auto/rest/tests/SimpleTest.java
@@ -0,0 +1,22 @@
+package qa.auto.rest.tests;
+
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SimpleTest {
+	
+	@Test
+	
+	public void getAllEmployee() {
+		assertTrue(false);
+	}
+
+@Test
+	
+	public void getAllEmployee1() {
+		assertTrue(true);
+	}
+}
